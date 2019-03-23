c1e52635fd9c593170ca30d34ac999a10d3df5be
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-03-23 21:09:27 +0100

    Created the color organizer app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import AddColorForm from './AddColorForm';
+import ColorList from './ColorList';
+import { v4 } from 'uuid';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      colors: []
+    }
+    this.addColor = this.addColor.bind(this);
+    this.removeColor = this.removeColor.bind(this);
+  }
+
+  addColor(title, color, rating) {
+    const colors = [
+      ...this.state.colors,
+      {
+        id: v4(),
+        title,
+        color,
+        rating
+      }
+    ]
+    this.setState({ colors })
+  }
+
+  removeColor(id) {
+    const colors = this.state.colors.filter(color =>
+      color.id !== id
+    )
+    this.setState({ colors })
+  }
+
   render() {
+    const { addColor, removeColor } = this;
+    const { colors } = this.state;
     return (
-      <div>
-        Hello first-timer
+      <div className="app">
+        <AddColorForm addNewColor={addColor} />
+        <ColorList colors={colors} removeColor={removeColor} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import colors from './data/colors';
 
-render(<App colors={colors} />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
